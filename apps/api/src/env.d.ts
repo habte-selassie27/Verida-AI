27c3c11780d4bda16433b8e173171aa0542a796f
codeXXripper <fafisru@gmail.com>
2026-06-24 19:22:23 +0300

    Use npm install instead of npm ci for proper workspace hoisting

--- apps/api/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'cors';
-declare module 'express';
-declare module 'express-async-handler';
-declare module 'helmet';
-declare module 'morgan';
-declare module 'multer';
-declare module 'jsonwebtoken';
-declare module 'ws';
